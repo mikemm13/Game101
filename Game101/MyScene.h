5894073422935dde583f5eccb97257e8fbc5ab0f
Miguel Martín-Nieto Morales <miguel@contxt.in>
2014-07-11 11:35:53 +0200

    Initial Commit

---
+++ Game101/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Game101
+//
+
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Game101Tests/Game101Tests.m
@@ -0,0 +1,34 @@
+//
+//  Game101Tests.m
+//  Game101Tests
+//
+//  Created by Miguel Martin Nieto on 11/07/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Game101Tests : XCTestCase
+
+@end
+
+@implementation Game101Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
